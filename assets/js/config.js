b782bf3090493b6fdf80bdcba051e25a354023c3
anshtradingcompanyraipur-coder <anshtradingcompanyraipur@gmail.com>
2026-05-08 11:10:38 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.GB_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://orsdrsbonzzngvmqbahn.supabase.co",
+  supabaseAnonKey: "sb_publishable_Q-66SqCIrJtyW-DCkWlmfg_OnWMTlIs",
   whatsappNumber: "918770327415"
 };
